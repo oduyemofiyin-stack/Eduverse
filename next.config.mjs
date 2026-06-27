b2da251ae3742138b439d8b76de5c9f21cb87180
oduyemofiyin-stack <oduyemofiyin@gmail.com>
2026-06-27 23:37:28 +0100

    Add images.pexels.com to remotePatterns in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'placehold.co' },
+      { protocol: 'https', hostname: 'images.pexels.com' },
     ],
   },
   async headers() {
